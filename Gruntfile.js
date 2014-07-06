f01fad6082d1f93bf9d2bd197b4e3f9d4bc34d5f
Jorge del Casar <jorge.casar@gmail.com>
2014-07-07 00:45:41 +0200

    Fix Twitter card

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,12 +28,33 @@ module.exports = function(grunt) {
                     to: '..'
                 }]
             }
+        },
+        push: {
+            options: {
+                files: ['package.json', 'bower.json'],
+                updateConfigs: [],
+                releaseBranch: false,
+                add: true,
+                addFiles: ['.'], // '.' for all files except ingored files in .gitignore
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json'], // '-a' for all files
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                npm: false,
+                npmTag: 'Release v%VERSION%',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-gh-pages');
     grunt.loadNpmTasks('grunt-text-replace');
+    grunt.loadNpmTasks('grunt-push-release');
 
     grunt.registerTask('build',  ['replace']);
     grunt.registerTask('deploy', ['gh-pages']);
