1e27a5e1f1e28caa5a23bd4008da4cad7bbd600e
Jorge del Casar <jorge.casar@gmail.com>
2014-07-12 23:00:02 +0200

    Release 0.1.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,76 +1,96 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        'connect': {
-            demo: {
-                options: {
-                    open: true,
-                    keepalive: true
-                }
-            }
-        },
-        'gh-pages': {
-            options: {
-                clone: 'bower_components/input-password'
-            },
-            src: [
-                'bower_components/**/*',
-                '!bower_components/input-password/**/*',
-                'demo/*', 'src/*', 'index.html', 'preview.png'
-            ]
-        },
-        'replace': {
-            example: {
-                src: ['src/*'],
-                dest: 'dist/',
-                replacements: [{
-                    from: 'bower_components',
-                    to: '..'
-                }]
-            }
-        },
-        'push': {
-            options: {
-                files: ['package.json', 'bower.json'],
-                updateConfigs: [],
-                push: {
-                    releaseBranch: ['develop', 'master']
-                },
-                add: true,
-                addFiles: ['.'], // '.' for all files except ingored files in .gitignore
-                commit: true,
-                commitMessage: 'Release %VERSION%',
-                commitFiles: ['-a'], // ['package.json', 'bower.json'], // '-a' for all files
-                createTag: true,
-                tagName: '%VERSION%',
-                tagMessage: 'Version %VERSION%',
-                push: true,
-                pushTo: 'origin',
-                npm: true,
-                npmTag: 'Release %VERSION%',
-                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
-            }
-        },
-        'uglify': {
-            compress: {
-                src: 'src/input-password.js',
-                dest: 'dist/input-password.js',
-                options: {
-                    compress: true,
-                    preserveComments: false
-                }
-            }
-        }
-    });
+	grunt.initConfig({
+		'connect': {
+			demo: {
+				options: {
+					open: true,
+					keepalive: true
+				}
+			}
+		},
+		'gh-pages': {
+			options: {
+				clone: 'bower_components/input-password'
+			},
+			src: [
+			'bower_components/**/*',
+			'!bower_components/input-password/**/*',
+			'demo/*', 'src/*', 'index.html', 'preview.png'
+			]
+		},
+		'replace': {
+			example: {
+				src: ['src/*'],
+				dest: 'dist/',
+				replacements: [{
+					from: 'bower_components',
+					to: '..'
+				}]
+			}
+		},
+		'push': {
+			options: {
+				files: ['package.json', 'bower.json'],
+				updateConfigs: [],
+				push: {
+					releaseBranch: ['develop', 'master']
+				},
+				add: true,
+				addFiles: ['.'],
+				commit: true,
+				commitMessage: 'Release %VERSION%',
+				commitFiles: ['-a'],
+				createTag: true,
+				tagName: '%VERSION%',
+				tagMessage: 'Version %VERSION%',
+				push: true,
+				pushTo: 'origin',
+				npm: true,
+				npmTag: 'Release %VERSION%',
+				gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+			}
+		},
+		'uglify': {
+			dist: {
+				files: {
+					'dist/input-password.js': 'src/input-password.js'
+				}
+			}
+		},
+		'cssmin': {
+			dist: {
+				files: {
+					'dist/input-password.css': 'src/input-password.css'
+				}
+			}
+		},
+		'htmlmin': {
+			dist: {
+				options: {
+					removeComments: true,
+					removeCommentsFromCDATA: true,
+					removeCDATASectionsFromCDATA: true,
+					collapseWhitespace: true,
+					conservativeCollapse: true
+				},
+				files: {
+					'dist/input-password.html': 'src/input-password.html'
+				}
+			}
+		}
+	});
 
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-gh-pages');
-    grunt.loadNpmTasks('grunt-text-replace');
-    grunt.loadNpmTasks('grunt-push-release');
+grunt.loadNpmTasks('grunt-contrib-connect');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-contrib-htmlmin');
+grunt.loadNpmTasks('grunt-gh-pages');
+grunt.loadNpmTasks('grunt-text-replace');
+grunt.loadNpmTasks('grunt-push-release');
 
-    grunt.registerTask('build',  ['replace', 'uglify']);
-    grunt.registerTask('deploy', ['gh-pages']);
-    grunt.registerTask('serve', ['connect']);
+grunt.registerTask('build',  ['replace', 'uglify', 'cssmin', 'htmlmin']);
+grunt.registerTask('deploy', ['gh-pages']);
+grunt.registerTask('serve', ['connect']);
 
 };
