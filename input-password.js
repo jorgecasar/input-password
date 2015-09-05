ac61f73db76c6ec7c6234d0bbba5d2ce6b670f0a
Jorge del Casar <jorge.casar@gmail.com>
2015-09-05 17:03:23 +0200

    doc(demo): improve doc and reorganice code

--- input-password.js
+++ input-password.js
@@ -1,8 +1,30 @@
 (function() {
-	var isReady = false;
-	return Polymer({
+	/**
+	 * input-password is an unstyled password field where user can show and hide the input value.
+	 *
+	 * Example:
+	 *
+	 *     <input-password visible toggleText="Show/Hide"></input-password>
+	 *
+	 * @element input-password
+	 * @homepage http://jorgecasar.github.io/input-password/
+	 * @demo
+	 */
+	Polymer({
 		is: 'input-password',
+
+		/**
+		 * Fired when the value of is shown.
+	  	 * @event showValue
+		 */
+
+		 /**
+		 * Fired when the value of is hidden.
+	  	 * @event hideValue
+		 */
+
 		properties: {
+
 			/**
 			 * Placeholder text that hints to the user
 			 * what can be entered in the input.
@@ -15,6 +37,7 @@
 				type: String,
 				value: ''
 			},
+
 			/**
 			 * If true, this input cannot be focused
 			 * and the user cannot change its value.
@@ -53,6 +76,7 @@
 				type: Boolean,
 				value: false
 			},
+
 			/**
 			 * The value of the input.
 			 *
@@ -64,6 +88,7 @@
 				type: String,
 				value: ''
 			},
+
 			/**
 			 * If true, the user can show the password.
 			 *
@@ -73,9 +98,9 @@
 			 */
 			visible: {
 				type: Boolean,
-				value: false,
-				observer: '_visibleChanged'
+				value: false
 			},
+
 			/**
 			 * The button text to show and hide the input value.
 			 *
@@ -87,6 +112,7 @@
 				type: String,
 				value: 'Toggle'
 			},
+
 			/**
 			 * This css class will be included
 			 * when the password is visible.
@@ -100,39 +126,67 @@
 				value: 'visible'
 			}
 		},
+
+		observers: [
+			'_visibleChanged(visible)'
+		],
+
 		listeners: {
 			'visibilityButton.tap': 'toggle'
 		},
 
-		/* -- Lifecycle ------------------------------------------------- */
 		attached: function() {
-			// Initialize attributes
 			this.showText = this.getShowText();
 			this.hideText = this.getHideText();
 			this._visibleChanged();
 		},
-		/* -- Observers ------------------------------------------------- */
+
+		/**
+		 * Observer for visible property
+		 */
 		_visibleChanged: function(){
 			this[this.visible ? 'showValue' : 'hideValue']();
 		},
-		/* -- Methods --------------------------------------------------- */
+
+		/**
+		 * Get the text of the button for show action
+		 */
 		getShowText: function(){
 			return this.toggleText.split('/')[0];
 		},
+
+		/**
+		 * Get the text of the button for hide action
+		 */
 		getHideText: function(){
 			return this.toggleText.split('/')[1] || this.showText;
 		},
+
+		/**
+		 * Action to show the input value
+		 */
 		showValue: function() {
+			this.visible = true;
 			this.$.input.type = 'text';
 			this.classList.add(this.visibleClass);
 			this.$.visibilityButton.innerHTML = this.hideText;
+			this.fire('showValue');
 		},
+
+		/**
+		 * Action to hide the input value
+		 */
 		hideValue: function() {
+			this.visible = false;
 			this.$.input.type = 'password';
 			this.classList.remove(this.visibleClass);
 			this.$.visibilityButton.innerHTML = this.showText;
+			this.fire('hideValue');
 		},
-		/* -- Listeners ------------------------------------------------- */
+
+		/**
+		 * Toggle the visibily of the input value
+		 */
 		toggle: function() {
 			this.visible = !this.visible;
 		}
